4d3655f497f5e5d13fcbfcd985407548814d5482
ben-denzler <bdenz001@ucr.edu>
2024-06-20 16:51:39 -0700

    Added status and description to Orders

--- src/app/order.ts
+++ src/app/order.ts
@@ -1,9 +1,17 @@
 import { Product } from "./product";
 
+export enum OrderStatus {
+  Designing = "Designing",
+  Packaging = "Packaging",
+  Finished = "Finished"
+}
+
 export interface Order {
   id: number,
   customer: string,
   school: string,
   price: number,
-  products: Product[]
+  products: Product[],
+  status: OrderStatus,
+  description: string
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Order } from '../order';
+import { Order, OrderStatus } from '../order';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 
@@ -23,35 +23,45 @@ export class TableComponent {
       customer: 'Benjamin Denzler',
       school: 'Century',
       price: 100,
-      products: []
+      products: [],
+      status: OrderStatus.Designing,
+      description: "This is the first order description."
     },
     {
       id: 1,
       customer: 'Alex Johnson',
       school: 'Riverside',
       price: 150,
-      products: []
+      products: [],
+      status: OrderStatus.Finished,
+      description: "This is the second order description."
     },
     {
       id: 2,
       customer: 'Samantha Bloom',
       school: 'Greenwood',
       price: 200,
-      products: []
+      products: [],
+      status: OrderStatus.Packaging,
+      description: "This is the third order description."
     },
     {
       id: 3,
       customer: 'Michael Reeves',
       school: 'Westview',
       price: 75,
-      products: []
+      products: [],
+      status: OrderStatus.Designing,
+      description: "This is the fourth order description."
     },
     {
       id: 4,
       customer: 'Linda Eastman',
       school: 'Sunrise',
       price: 125,
-      products: []
+      products: [],
+      status: OrderStatus.Finished,
+      description: "This is the fifth order description."
     }
   ];
 }
