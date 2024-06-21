8fc41f095cd944c8d8d069eda837eb9479a319de
ben-denzler <bdenz001@ucr.edu>
2024-06-20 18:01:18 -0700

    Added table for items in order

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -23,7 +23,10 @@ export class TableComponent {
       customer: 'Benjamin Denzler',
       school: 'Century',
       price: 100,
-      products: [],
+      products: [
+        { id: 0, name: 'T-shirt', quantity: 2 },
+        { id: 1, name: 'Pants', quantity: 1 }
+      ],
       status: OrderStatus.Designing,
       description: "This is the first order description.",
       createdOn: new Date(2022, 0, 1), // January 1, 2022
@@ -34,7 +37,10 @@ export class TableComponent {
       customer: 'Alex Johnson',
       school: 'Riverside',
       price: 150,
-      products: [],
+      products: [
+        { id: 2, name: 'Shoes', quantity: 1 },
+        { id: 3, name: 'Socks', quantity: 3 }
+      ],
       status: OrderStatus.Finished,
       description: "This is the second order description.",
       createdOn: new Date(2022, 0, 3), // January 3, 2022
@@ -45,7 +51,10 @@ export class TableComponent {
       customer: 'Samantha Bloom',
       school: 'Greenwood',
       price: 200,
-      products: [],
+      products: [
+        { id: 4, name: 'Backpack', quantity: 1 },
+        { id: 5, name: 'Notebook', quantity: 5 }
+      ],
       status: OrderStatus.Packaging,
       description: "This is the third order description.",
       createdOn: new Date(2022, 0, 5), // January 5, 2022
@@ -56,7 +65,10 @@ export class TableComponent {
       customer: 'Michael Reeves',
       school: 'Westview',
       price: 75,
-      products: [],
+      products: [
+        { id: 6, name: 'Pencil', quantity: 10 },
+        { id: 7, name: 'Eraser', quantity: 5 }
+      ],
       status: OrderStatus.Designing,
       description: "This is the fourth order description.",
       createdOn: new Date(2022, 0, 7), // January 7, 2022
@@ -67,7 +79,10 @@ export class TableComponent {
       customer: 'Linda Eastman',
       school: 'Sunrise',
       price: 125,
-      products: [],
+      products: [
+        { id: 8, name: 'Jacket', quantity: 1 },
+        { id: 9, name: 'Hat', quantity: 2 }
+      ],
       status: OrderStatus.Finished,
       description: "This is the fifth order description.",
       createdOn: new Date(2022, 0, 9), // January 9, 2022
